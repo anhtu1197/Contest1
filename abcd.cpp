0381e95e9c7cf6b0df3a382e0459b0798f1bd535
anhtu1197 <anhtu1197@gmail.com>
2017-08-20 11:36:53 +0700

    1
    asdf

---
+++ abcd.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define F(i, n, m) for (int i = n; i <= m; i++)
+#define Fa(i, n, m) for (int i = n; i>= m; i--)
+#define ll long long
+using namespace std;
+main () {
+	int b = 10;
+	int &a;
+	a = b;
+	cout << a;
+}
+
